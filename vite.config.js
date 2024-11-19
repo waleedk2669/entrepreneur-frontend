e805e13ef43ab0f3ab61011f671ed73035e53ba3
Muhammad Waleed <waleedk2669@gmail.com>
2024-11-20 00:57:31 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:5000", // Flask backend URL
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
-      },
-    },
-  },
+  // server: {
+  //   port: 5173,
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://127.0.0.1:5000", // Flask backend URL
+  //       secure: false,
+  //       rewrite: (path) => path.replace(/^\/api/, "/api"),
+  //     },
+  //   },
+  // },
 });
